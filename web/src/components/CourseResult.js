d1a813208d1fcc7516077ca7efe5d5761e02aef0
Paul Boeck <boeck87+git@gmail.com>
2021-07-21 17:23:29 +0200

    Add more course information

--- web/src/components/CourseResult.js
+++ web/src/components/CourseResult.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
-function CourseResult({course}) {
-    let c = course._source;
-  return(
-    <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+function CourseResult({ course }) {
+  let c = course._source;
+  return (
+    <div className="tc dib br3 pa3 ma2 grow mw7 bg-washed-yellow bw2 shadow-5">
       <div>
-        <p>{c.name}</p>
+        <h3>{c.name}</h3>
+        <h4>{c.untertitel}</h4>
+
+        {
+          c.text.map(t => <p>{t.text}</p>)
+        }
+        <p>{c.schlagwort.join(' | ')}</p>
+        <a href={c.webadresse.uri} target="_blank" rel="noopener noreferrer">{c.webadresse.name}</a>
+
       </div>
     </div>
   );
